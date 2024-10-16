ee3a90740fce7bcfa1d0139dd0f758c9b859ed02
Richard Karoki <karokirichard522@gmail.com>
2024-10-16 17:27:23 +0300

    Create FooterSection.vue

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="bg-light text-center py-3">
+    <p>&copy; 2024 Animal Kingdom. All Rights Reserved.</p>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
