c338581d4f345e43be715558ea3c8b96746b1b88
Richard Karoki <karokirichard522@gmail.com>
2024-10-16 17:27:48 +0300

    Create HomePage.vue

---
+++ src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NavbarSection />
+    <HeroSection />
+  </div>
+</template>
+
+<script>
+import HeroSection from "@/components/HeroSection.vue";
+export default {
+  name: "HomePage",
+  components: {
+    HeroSection,
+  },
+};
+</script>
+
+<style>
+/* Custom styles for the home page */
+</style>
