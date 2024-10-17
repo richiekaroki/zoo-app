f749b30d0f97fc1c659982c4981ee2a513c35870
Richard Karoki <karokirichard522@gmail.com>
2024-10-17 22:13:05 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 
 createApp(App).use(router).mount("#app");
